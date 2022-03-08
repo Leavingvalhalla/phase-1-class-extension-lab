411b12692ceb01c48528101cb2eb69401561a8cd
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-08 15:57:00 -0800

    commit tree e4f959c0e7895112ca738ce61a1df82e1027f4da

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((total, side) => total + side, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    const sortedSides = this.sides.sort();
+    return sortedSides[0] + sortedSides[1] > sortedSides[2];
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    for (let side of this.sides) {
+      if (side != this.sides[0]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  get area() {
+    console.log(this.sides);
+    return this.sides[0] ** 2;
+  }
+}
